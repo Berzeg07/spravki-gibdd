02a6ace0a6f291d10042e46e334079e0668e27d3
Aslan <berezg86@gmail.com>
2020-08-17 13:36:32 +0300

    Верстка шапки

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-    alert(1);
+$(document).ready(function () {
+
+    $(".burger").click(function () {
+        $(this).toggleClass('is-active');
+        $('.dropdown-menu').toggleClass('is-active');
+    });
+
+
+    $(".mobnav-list a").click(function () {
+
+        var $this = $(this);
+        var elem = $(this).next();
+
+        if (!$this.hasClass("is-active")) {
+            $(".mobnav-list__dropdown").slideUp();
+            $(".mobnav-list a").removeClass("is-active");
+        }
+
+        $this.toggleClass("is-active");
+        elem.slideToggle();
+
+    });
+
 });
